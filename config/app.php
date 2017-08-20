c8b47f811b247f2b3c33bf924fbdace604137a63
Jia Yee <jiayeerawr@gmail.com>
2017-08-20 16:59:32 +0000

    Require Laravel-Facebook PHP SDK
    Signed-off-by: Jia Yee <jiayeerawr@gmail.com>

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /**
+         * Laravel-Facebook PHP SDK Service Providers...
+         */
+        SammyK\LaravelFacebookSdk\LaravelFacebookSdkServiceProvider::class,
+
     ],
 
     /*
@@ -225,6 +230,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Facebook' => SammyK\LaravelFacebookSdk\FacebookFacade::class,
 
     ],
 
