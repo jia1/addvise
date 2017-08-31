ee299ce49997abe05efbfd7798d569f7be7ed38d
Jia Yee <jiayeerawr@gmail.com>
2017-08-31 18:08:05 +0000

    Add remaining logic to retrieve #needAddvise created by the user
    Also fix redundant $data and replace with $advice_requests in the index action
    
    Signed-off-by: Jia Yee <jiayeerawr@gmail.com>

--- resources/views/advisees/requests/show.blade.php
+++ resources/views/advisees/requests/show.blade.php
@@ -6,7 +6,6 @@
 
 <div class="content">
     @foreach(json_decode($advice_requests, true) as $post)
-        {{--
         <p>{{ $post['created_time'] }}</p>
         <p>{{ $post['message'] }}</p>
         <p>Facebook ID: {{ $post['id'] }}</p>
@@ -14,8 +13,6 @@
         @foreach($post['comments'] as $comment)
             <p>{{ $comment }}</p>
         @endforeach
-        --}}
-        <p>{{ $post['fb_post_id'] }}</p>
     @endforeach
 </div>
 
