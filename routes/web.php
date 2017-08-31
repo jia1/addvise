c07b319cd73957c17dd9ac296b1d13c70fe78b87
Jia Yee <jiayeerawr@gmail.com>
2017-08-31 05:27:58 +0000

    Add relevant routes for advisors, add show route for advisee
    Also add documentation to routes
    
    Signed-off-by: Jia Yee <jiayeerawr@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,44 @@
 Route::get('/', 'PagesController@getWelcome');
 
 Route::get('home', 'PagesController@getHome')->middleware('facebook');
+
+/**
+ * Pages Controller
+ */
+
+// User settings
 Route::get('settings', 'PagesController@getSettings')->middleware('facebook');
 
+/**
+ * Advisees Controller
+ */
+
+// #needAddvise feed
 Route::get('needAddvise', 'AdviseesController@getTakeAdviceIndex')->middleware('facebook');
+
+// View for creating a new #needAddvise
 Route::get('needAddvise/new', 'AdviseesController@getTakeAdviceNew')->middleware('facebook');
+
+// Post to Facebook and store relevant identification in database
 Route::post('needAddvise/create', 'AdviseesController@postTakeAdviceCreate')->middleware('facebook');
+
+// View for all #needAddvise created by the user
+Route::get('needAddvise/me', 'AdviseesController@getTakeAdviceShow')->middleware('facebook');
+
+
+/**
+ * Advisors Controller
+ */
+
+// All advice given (with or without the corresponding #needAddvise?)
+Route::get('advice', 'AdvisorsController@getGiveAdviceIndex')->middleware('facebook');
+
+// View for creating a new advice for an existing #needAddvise
+Route::get('{id}/advice/new', 'AdvisorsController@getGiveAdviceNew')->middleware('facebook');
+
+// Comment on Facebook and store relevant identifcation in database
+Route::post('{id}/advice/create', 'AdvisorsController@postGiveAdviceCreate')->middleware('facebook');
+
+// View for all advice given by the user
+Route::get('advice/me', 'AdvisorsController@getGiveAdviceShow')->middleware('facebook');
 
