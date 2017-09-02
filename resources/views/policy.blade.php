0c47157c69e8f4fc3a3493105dbd233d46a7133a
Daphne <won.jr@hotmail.com>
2017-09-03 01:46:34 +0800

    Fixing Policy List CSS not ordered properly

--- resources/views/policy.blade.php
+++ resources/views/policy.blade.php
@@ -10,7 +10,7 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-        <link rel="stylesheet" href="/css/main.css" />
+        <link rel="stylesheet" href="/css/policy.css" />
         <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
         <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
     </head>
@@ -35,7 +35,7 @@
 
             <!-- Main -->
             <div id="main">
-                <div id="policy">
+                <div id="policy" style="overflow: auto;">
                     <center><h1>Addvise Terms of Service</h1></center>
 
                     <h2>1. Terms</h2>
