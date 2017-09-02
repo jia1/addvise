3a7a234558f656ae436ca6f74fc242444cf8fceb
Daphne <won.jr@hotmail.com>
2017-09-02 21:51:24 +0800

    Added popup javascript for no javascript detected

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,5 +1,8 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
+    <head>
+        <link rel="stylesheet" href="/css/popout.css" />
+    </head>
     <body>
         @include('partials._facebook')
         <div class="flex-center position-ref full-height">
@@ -7,6 +10,23 @@
             @yield('content')
             @include('partials._footer')
         </div>
+
+        <!--POPUP-->
+        <div id="nojs" class="overlay light">
+        <a class="cancel" href="#"></a>
+        <div class="nojscontent">
+            <h2>You need to turn on your JavaScript to access Addvise.</h2>
+        </div>
+
+        <!-- SCRIPT TO CHECK IF JAVASCRIPT EXISTS -->
+        <noscript>
+            <style type="text/css">
+                .pagecontainer {display:none;}
+            </style>
+            <div class="noscriptmsg">
+                <!-- JAVASCRIPT BLOCKED -->
+                <meta http-equiv="refresh" content="2; url=./#nojs" />
+            </div>
+        </noscript>
     </body>
 </html>
-
