1124126a43d80d616a0da2b377ce36401066b6a0
Apoorva Ullas <apoorva_ullas@yahoo.com.sg>
2017-09-02 21:19:26 +0800

    Remove double hyphen

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -1,6 +1,6 @@
 <div class="top-right links">
     <div class="nav-external" style="display: none;">
-        <button id = "login" class="loginBtn loginBtn--facebook">
+        <button id = "login" class="loginBtn loginBtn-facebook">
   			Login with Facebook
 		</button>
     </div>
@@ -107,18 +107,18 @@ body { padding: 2em; }
 
 
 /* Facebook */
-.loginBtn--facebook {
+.loginBtn-facebook {
   background-color: #4C69BA;
   background-image: linear-gradient(#4C69BA, #3B55A0);
   /*font-family: "Helvetica neue", Helvetica Neue, Helvetica, Arial, sans-serif;*/
   text-shadow: 0 -1px 0 #354C8C;
 }
-.loginBtn--facebook:before {
+.loginBtn-facebook:before {
   border-right: #364e92 1px solid;
   background: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/14082/icon_facebook.png') 6px 6px no-repeat;
 }
-.loginBtn--facebook:hover,
-.loginBtn--facebook:focus {
+.loginBtn-facebook:hover,
+.loginBtn-facebook:focus {
   background-color: #5B7BD5;
   background-image: linear-gradient(#5B7BD5, #4864B1);
 }
