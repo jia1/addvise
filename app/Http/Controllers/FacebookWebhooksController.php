1808e87460ad75328e7353e1e2280970102d8755
Jia Yee <jiayeerawr@gmail.com>
2017-09-06 11:39:16 +0000

    Fix FacebookWebhooksController and update .env.example
    1. Fix method call for getting HTTP request method.
    2. Correctly retrieve JSON payload as array.
    3. Add field for webhook verify token.
    4. Remove other unused fields (variations of page ID).
    
    Signed-off-by: Jia Yee <jiayeerawr@gmail.com>

--- app/Http/Controllers/FacebookWebhooksController.php
+++ app/Http/Controllers/FacebookWebhooksController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Log;
 
 class FacebookWebhooksController extends Controller
 {
     public function notify(Request $request) {
-        if (\Request::getMethod('get')) {
+        if (\Request::isMethod('get')) {
             $mode = $request->input('hub_mode');
             $challenge = $request->input('hub_challenge');
             $verify_token = $request->input('hub_verify_token');
@@ -15,12 +16,13 @@ class FacebookWebhooksController extends Controller
                 return response($challenge, 200);
             }
         } else {
-            if (\Request::getMethod('post')) {
+            if (\Request::isMethod('post')) {
                 if ($request->header('Content-Type') === 'application/json') {
                     $sha1_header = $request->header('X-Hub-Signature');
                     $sha1_signature = substr($sha1_header, 5);
                     if ($sha1_signature === sha1(env('FACEBOOK_APP_SECRET', ''))) {
-                        $data = json_decode($request->payload, true);
+                        $request_arr = $request->json()->all();
+
                         return response('OK', 200);
                     }
                 }
