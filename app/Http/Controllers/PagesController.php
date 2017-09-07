2f886598ada51b9d342cc2b32c0fcfec2792dee9
Jia Yee <jiayeerawr@gmail.com>
2017-09-07 18:37:23 +0000

    Add site stats to home page
    Signed-off-by: Jia Yee <jiayeerawr@gmail.com>

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use DB;
+use Redis;
+
 class PagesController extends Controller {
     // READ: Welcome page for everyone
     public function getWelcome() {
@@ -9,7 +12,22 @@ class PagesController extends Controller {
     }
 
     public function getHome() {
-        return view('home');
+        $db_arr = Redis::info()['Keyspace'];
+        $num_recent_advisees = 0;
+        if (count($db_arr) > 0) {
+            foreach ($db_arr as $db_name=>$db_stat) {
+                $num_recent_advisees += count($db_stat['keys']);
+            }
+        }
+        $num_users = DB::table('users')->count();
+        $num_advice_requests = DB::table('advice_requests')->count();
+        $num_advice_given = DB::table('advice_given')->count(); 
+        return view('home', [
+            'num_recent_advisees' => $num_recent_advisees,
+            'num_users' => $num_users,
+            'num_advice_requests' => $num_advice_requests,
+            'num_advice_given' => $num_advice_given,
+        ]);
     }
 
     public function getAsk(){
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,6 +14,15 @@
         <header class="major">
             <h2>Home</h2>
         </header>
+
+        <header class="major">
+            <h3>Addvise Stats</h3>
+        </header>
+
+        <p>Number of requests for advice in the last 5 minutes: {{ $num_recent_advisees }}</p>
+        <p>Registered users: {{ $num_users }}</p>
+        <p>Total requests for advice: {{ $num_advice_requests }}</p>
+        <p>Total advice given: {{ $num_advice_given }}</p>
     </section>
 
 </div>
